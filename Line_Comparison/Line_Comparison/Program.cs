915a0bf9a11772fe2b490bde41f5c1e0e5e619b1
rajashri03 <92008095+rajashri03@users.noreply.github.com>
2021-12-21 17:58:48 +0530

    Update Program.cs

--- Line_Comparison/Line_Comparison/Program.cs
+++ Line_Comparison/Line_Comparison/Program.cs
@@ -13,5 +13,4 @@ Console.WriteLine("Enter values of the coordinates for second point(x2,y2)");
 x2 = Convert.ToInt32(Console.ReadLine());
 y2 = Convert.ToInt32(Console.ReadLine());
 length =Math.Sqrt(Math.Pow(x2 - x1, 2)) + (Math.Pow(y2 - y1, 2)); //Calculating Length
-len = Math.Round(length, 2);
 Console.WriteLine("Length of the line is: " + length);
